8c85d13e9db8a5d20e75184ad5d76478e6a21253
PriyaQA@24*7 <a.priyadas15@gmail.com>
2025-05-13 08:43:46 +0530

    13-05-2025 pushed the file

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -1,7 +1,8 @@
 package ex_01_Java_Basic;
 
 public class Lab001_HelloWorldProgram {
-    public static void main(string args[]){
-        system.out.println("Hello World - Lab001");
+    public static void main(String[] args) {
+        System.out.println("Hello World Lab001");
     }
-}
+    }
+
